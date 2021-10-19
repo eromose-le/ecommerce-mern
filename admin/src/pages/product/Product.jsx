20d1573fd59b715758c37dda7271f2aee2cb028c
eromose-le <erons24@yahoo.com>
2021-10-19 10:20:19 +0100

    getSingle product details and income stats

--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -1,10 +1,59 @@
 import { Publish } from '@material-ui/icons';
-import { Link } from 'react-router-dom';
+import { useEffect, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
 import Chart from '../../components/chart/Chart';
-import { productData } from '../../dummyData';
+import { userRequest } from '../../requestMethods';
 import './product.css';
 
 const Product = () => {
+  const location = useLocation();
+  const productId = location.pathname.split('/')[2];
+  const [pStats, setPStats] = useState([]);
+
+  const product = useSelector((state) =>
+    state.product.products.find((product) => product._id === productId)
+  );
+
+  const MONTHS = useMemo(
+    () => [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Agu',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get('orders/income?pid=' + productId);
+        const list = res.data.sort((a, b) => {
+          return a._id - b._id;
+        });
+        list.map((item) =>
+          setPStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], Sales: item.total }
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, [productId, MONTHS]);
+
+  console.log('OUT', pStats);
   return (
     <div className="product">
       {/* Header */}
@@ -17,20 +66,16 @@ const Product = () => {
       {/* Top */}
       <div className="productTop">
         <div className="productTopLeft">
-          <Chart data={productData} dataKey="Sales" title="Sales Performance" />
+          <Chart data={pStats} dataKey="Sales" title="Sales Performance" />
         </div>
         <div className="productTopRight">
           <div className="productInfoTop">
-            <img
-              src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt="product"
-              className="productInfoImg"
-            />
-            <span className="productName">Apple Airpods</span>
+            <img src={product?.img} alt="product" className="productInfoImg" />
+            <span className="productName">{product?.title}</span>
           </div>
           <div className="productInfoBottom">
             <div className="productInforItem">
-              <span className="productInfoKey">id:</span>
+              <span className="productInfoKey">{product?._id} :</span>
               <span className="productInfoValue">123</span>
             </div>
             <div className="productInforItem">
@@ -38,12 +83,8 @@ const Product = () => {
               <span className="productInfoValue">5123</span>
             </div>
             <div className="productInforItem">
-              <span className="productInfoKey">active:</span>
-              <span className="productInfoValue">yes</span>
-            </div>
-            <div className="productInforItem">
               <span className="productInfoKey">in stock:</span>
-              <span className="productInfoValue">no</span>
+              <span className="productInfoValue">{product?.inStock}</span>
             </div>
           </div>
         </div>
@@ -54,23 +95,22 @@ const Product = () => {
           {/* Form Left */}
           <div className="productFormLeft">
             <label>Product Name</label>
-            <input type="text" placeholder="Apple Airpod" />
+            <input type="text" placeholder={product?.title} />
+            <label>Product Description</label>
+            <input type="text" placeholder={product?.desc} />
+            <label>Price</label>
+            <input type="text" placeholder={product?.price} />
             <label>In stock</label>
             <select name="instock" id="idstock">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
-            <label>Active</label>
-            <select name="active" id="idactive">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
             </select>
           </div>
           {/* Form Right */}
           <div className="productFormRight">
             <div className="productUpload">
               <img
-                src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                src={product?.img}
                 alt="product"
                 className="productUploadImg"
               />
